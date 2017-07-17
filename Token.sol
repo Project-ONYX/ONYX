5ad7d086066c2017339344dc0f2946f62fe8b170
Zain Admani <zain.admani@gmail.com>
2017-07-17 12:17:18 -0500

    V0 of the ONYX token is complete. Voting and Vote calling has been implemented but not tested.

--- Token.sol
+++ Token.sol
@@ -8,34 +8,51 @@ contract OnyxToken {
     uint256 totalSupply = 0;
 	mapping (address => uint256) balances;
 	mapping (address => mapping (address => uint256)) allowances;
-	mapping (address => mapping (string => bool)) voteCalled;
-	mapping (address => mapping (string => bool)) voted;
+
+	struct Vote {
+		uint256 blockNum;
+		uint256 value;
+	}
+
+	struct VoteCounter {
+		uint256 num;
+		uint256 totalValue;
+	}
+
+	mapping (address => mapping (string => uint256)) voteCalled;
+	mapping (address => mapping (string => Vote)) voted;
 
 	mapping (string => uint) voteTypes;
 	voteTypes["Fee"] = 1;
 	voteTypes["Stake"] = 2;
 	voteTypes["Milestone"] = 3;
 
-	mapping (string => uint256) voteCalls;
-	mapping (string => uint256) votes;
+	string[3] voteTypesArr;
+	voteTypesArr[0] = "Fee";
+	voteTypesArr[1] = "Stake";
+	voteTypesArr[2] = "Milestone";
 
-	// Voting explanation:
-	// When calling vote, voteCalled is moved to true and voteCalls is incremented by the number of tokens address has.
-	// 	if the address transfers tokens and voteCalled is true then the amount of tokens transfered is "unvoteCalled" by subtracting it from voteCalls to clean token (need to check if recipient voteCalled in which case don't)
-	// Mirroring situation for voting also.
+	mapping (string => uint256) voteCalls;
+	mapping (string => VoteCounter) votes;
+
+	mapping (string => bool) votingActive;
+	votingActive["Fee"] = false;
+	votingActive["Stake"] = false;
+	votingActive["Milestone"] = false;
 
 	event Transfer(address indexed _from, address indexed _to, uint _value);
 	event Approval(address indexed _owner, address indexed _spender, uint _value);
 	event CallVote(address indexed _owner, string indexed _voteType);
-	event Vote(address indexed _ownder, string indexed _voteType, uint _numVotes, uint _vote);
+	event Vote(address indexed _owner, string indexed _voteType, uint _numVotes, uint256 _vote);
 
-	function OnyxToken(uint _callToVotePercent, uint _votePassPercent, uint _votingBlockWindowSize) {
+	function OnyxToken(uint _callToVotePercent, uint _votePassPercent, uint _votingBlockWindowSize, uint _fee) {
 		if (_callToVotePercent == 0) throw;
 		if (_votePassPercent == 0) throw;
 		if (_votingBlockWindowSize == 0) throw;
 
-		callToVotePercent = _callToVotePercent; // Percent of tokens calling a vote required to trigger vote
-		votePassPercent = _votePassPercent; // Percent of tokens voting required to pass a vote
+		fee = _fee;
+		callToVotePercent = _callToVotePercent; // Percent of tokens calling a vote required to trigger vote (divide by 100)
+		votePassPercent = _votePassPercent; // Percent of tokens voting required to pass a vote (divide by 100)
 		votingBlockWindowSize = _votingBlockWindowSize; // Window for collecting vote calls
 		currentVoteBlock = block.number; // Current block window start block
 		endVoteBlock = currentVoteBlock + votingBlockWindowSize; // End of current vote window block
@@ -50,13 +67,20 @@ contract OnyxToken {
 	}
 	
 	function transfer(address _to, uint _value) returns (bool success) {
-		if(balances[msg.sender] >= _amount 
-			&& _amount > 0 
-			&& balance[_to] + _amount > balances[_to]) {
+		if(balances[msg.sender] >= _value 
+			&& _value > 0 
+			&& balance[_to] + _value > balances[_to]) {
 
-			balances[msg.sender] -= _amount;
-			balances[_to] += _amount;
-			Transfer(msg.sender, _to, _amount);
+			balances[msg.sender] -= _value;
+			balances[_to] += _value;
+
+			checkVoteBlock();
+			for(uint x = 0; x < voteTypesArr.length; x++) {
+				type = voteTypesArr[x];
+				transferVoteCall(msg.sender, _to, type, _value);
+				transferVote(msg.sender, _to, type, _value);
+			}
+			Transfer(msg.sender, _to, _value);
 			return true;
 		} else {
 			return false;
@@ -66,13 +90,21 @@ contract OnyxToken {
 	function transferFrom(address _from, address _to, uint _value) returns (bool success) {
 		if(balances[_from] >= _value 
 			&& allowances[_from][msg.sender] >= _value 
-			&& _amount > 0
-			&& balances[_to] + _amount > balances[_to]) {
+			&& _value > 0
+			&& balances[_to] + _value > balances[_to]) {
 
-			balances[_from] -= _amount;
-			allowances[_from][msg.sender] -= _amount;
-			balances[_to] += _amount;
-			Transfer(_from, _to, _amount);
+			balances[_from] -= _value;
+			allowances[_from][msg.sender] -= _value;
+			balances[_to] += _value;
+
+			checkVoteBlock();
+			for(uint x = 0; x < voteTypesArr.length; x++) {
+				type = voteTypesArr[x];
+				transferVoteCall(msg.sender, _to, type, _value);
+				transferVote(msg.sender, _to, type, _value);
+			}
+
+			Transfer(_from, _to, _value);
 			return true;
 		} else {
 			return false;
@@ -88,11 +120,126 @@ contract OnyxToken {
 		return allowances[_owner][_spender];
 	}
 
-	function callVote(uint _type) returns (bool success) {
-
+	function transferVoteCall(address _owner, address _transfer, string _type, uint _value) returns (bool success) {
+		if(hasCalledVote(_owner, _type)) {
+			voteCalls[_type] -= _value;
+		}
+		if(hasCalledVote(_transfer, _type)) {
+			voteCalls[_type] += _value;
+			voteCalled[_transfer][_type] = block.number;
+		}
+		return true;
 	}
 
-	function castVote(uint _type) returns (bool success) {
+	function transferVote(address _owner, address _transfer, string _type, uint _value) returns (bool success) {
+		if(hasVoted(_owner, _type)) {
+			votes[_type].num -= _value;
+			votes[_type].totalValue -= _value*voted[_owner][_type].vote;
+		}
+		if(hasVoted(_transfer, _type)) {
+			votes[_type].num += _value;
+			votes[_type].totalValue += _value*voted[_transfer][_type].vote;
+		}
+		return true;
+	}
 
+	function hasCalledVote(address _owner, string _type) constant returns (bool truth) {
+		if(voteCalled[_owner][_type] >= currentVoteBlock && voteCalled[_owner][_type] < endVoteBlock) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function hasVoted(address _owner, string _type) constant returns (bool truth) {
+		if(voted[_owner][_type].blockNum >= currentVoteBlock && voted[_owner][_type].blockNum < endVoteBlock) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function isVotingActive(string _type) constant returns (bool active) {
+		return votingActive[_type];
+	}
+
+	function checkVoteBlock() returns (bool success) {
+		if(block.number >= endVoteBlock) {
+			currentVoteBlock = endVoteBlock;
+			endVoteBlock = currentVoteBlock + votingBlockWindowSize;
+
+			if(votingActive["Fee"]) {
+				if(votes["Fee"]/totalSupply >= votePassPercent/100) {
+					// Apply vote
+				}
+				votingActive["Fee"] = false;
+			} else {
+				if(voteCalls["Fee"]/totalSupply >= callToVotePercent/100) {
+					votingActive["Fee"] = true;
+					votes["Fee"] = VoteCounter(0, 0);
+				}
+			}
+
+			if(votingActive["Stake"]) {
+				if(votes["Stake"]/totalSupply >= votePassPercent/100) {
+					// Apply vote
+				}
+				votingActive["Stake"] = false;
+			} else {
+				if(voteCalls["Stake"]/totalSupply >= callToVotePercent/100) {
+					votingActive["Stake"] = true;
+					votes["Stake"] = VoteCounter(0, 0);
+				}
+			}
+
+			if(votingActive["Milestone"]) {
+				if(votes["Milestone"]/totalSupply >= votePassPercent/100) {
+					// Apply vote
+				}
+				votingActive["Milestone"] = false;
+			} else {
+				if(voteCalls["Milestone"]/totalSupply >= callToVotePercent/100) {
+					votingActive["Milestone"] = true;
+					votes["Milestone"] = VoteCounter(0, 0);
+				}
+			}
+
+			voteCalls["Fee"] = 0;
+			voteCalls["Stake"] = 0;
+			voteCalls["Milestone"] = 0;
+			returns true;
+		} else {
+			returns false;
+		}
+	}
+
+	function callVote(string _type) returns (bool success) {
+		checkVoteBlock();
+		if(!votingActive[_type] && voteTypes[_type] > 0 && !hasCalledVote(msg.sender, _type)) {
+			voteCalled[msg.sender][_type] = block.number;
+			voteCalls[_type] += balances[msg.sender];
+			CallVote(msg.sender, _type);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function castVote(string _type, uint256 _vote) returns (bool success) {
+		checkVoteBlock();
+		if(votingActive[_type] && _vote >= 0) {
+			if(!hasVoted(msg.sender, _type)) {
+				votes[_type].num += balances[msg.sender];
+				votes[_type].totalValue += balances[msg.sender]*_vote;
+			} else {
+				votes[_type].totalValue -= balances[msg.sender]*(voted[msg.sender][_type].value - _vote);
+			}
+			vote = Vote(block.number, _vote);
+			voted[msg.sender][_type] = vote;
+			Vote(msg.sender, _type, balances[msg.sender], _vote);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
